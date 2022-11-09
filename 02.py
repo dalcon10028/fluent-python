1f8770a17f66b7e46c32de8164ada15cb0c2241d
corn <dalcon10280@gmail.com>
2022-11-10 02:04:49 +0900

    02

--- 02.py
+++ 02.py
@@ -62,3 +62,25 @@ print(filename)
 # 전통적으로 _는 gettext.gettext()함수의 대한 별명으로 사용된다. 그 외의 경우에는 _를 플레이스홀더로 사용하는 것이 좋다.
 
 # 초과 항목을 잡기 위해 * 사용하기
+# js의 spread syntax랑 비슷한거 같음
+a, b, *rest = range(5)
+print(a, b, rest)
+
+a, *body, c, d = range(5)
+print(a, body, c, d)
+
+# 내포된 튜플 언패킹
+metro_areas = [
+  ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+  ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+  ('Tokyo', 'JP', 20.142, (19.433333, -99.133333)),
+  ('Tokyo', 'JP', 20.104, (40.808611, -74.020386)),
+  ('Tokyo', 'JP', 19.649, (-23.547778, -46.635833)), # trailing comma를 지원하는군
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+# 마지막 필드를 튜플에 할당하면서 언패킹 - js의 구조분해할당 비슷함 (destructuring assignment)
+for name, cc, pop, (latitude, longitude) in metro_areas:
+  if longitude <= 0:
+    print(fmt.format(name, latitude, longitude))
