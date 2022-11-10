1b931e72cb89592c58fd3bfc5e05894f8c97e2c4
dalcon10028 <dalcon10280@gmail.com>
2022-11-11 01:08:40 +0900

    02

--- 02.py
+++ 02.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+import os
+import array
 symbols = "ÄÄ123213"
 
 codes = [ord(symbol) for symbol in symbols]
@@ -22,7 +25,6 @@ print(tshirts)
 # 다른 생성자에 전달할 리스트를 통쨰로 만들지 않고 이터레이터 프로토콜을 이용해서 항목을 하나씩 생성하는 제너레이터 표현식은 메모리를 더 적게 사용한다.
 # 제너레이터 표현식은 지능현 리스트와 동일한 구문을 사용하지만 대괄호 대신 괄호를 사용한다.
 tuple(ord(symbol) for symbol in symbols)
-import array
 print(array.array('I', (ord(symbol) for symbol in symbols)))
 
 # 2.3
@@ -34,14 +36,14 @@ lax_coodinates = (33.9425, -118.408056)
 city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
 traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
 for passport in sorted(traveler_ids):
-  print('%s/%s' % passport)
+    print('%s/%s' % passport)
 
 for country, _ in traveler_ids:
-  print(country)
+    print(country)
 
 # 병렬할당 (parallel assignment)
 lax_coodinates = (33.9425, -118.408056)
-latitude, longtitude = lax_coodinates # 튜플 언패킹
+latitude, longtitude = lax_coodinates  # 튜플 언패킹
 print(latitude)
 print(longtitude)
 
@@ -55,7 +57,6 @@ print(a, b)
 print(*(1, 2))
 
 # 파일 시스템경로에서 경로명과 파일명 가져오기
-import os
 _, filename = os.path.split('/home/aaa/bbb')
 print(filename)
 
@@ -71,19 +72,19 @@ print(a, body, c, d)
 
 # 내포된 튜플 언패킹
 metro_areas = [
-  ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
-  ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
-  ('Tokyo', 'JP', 20.142, (19.433333, -99.133333)),
-  ('Tokyo', 'JP', 20.104, (40.808611, -74.020386)),
-  ('Tokyo', 'JP', 19.649, (-23.547778, -46.635833)), # trailing comma를 지원하는군
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Tokyo', 'JP', 20.142, (19.433333, -99.133333)),
+    ('Tokyo', 'JP', 20.104, (40.808611, -74.020386)),
+    ('Tokyo', 'JP', 19.649, (-23.547778, -46.635833)),  # trailing comma를 지원하는군
 ]
 
 print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
 fmt = '{:15} | {:9.4f} | {:9.4f}'
 # 마지막 필드를 튜플에 할당하면서 언패킹 - js의 구조분해할당 비슷함 (destructuring assignment)
 for name, cc, pop, (latitude, longitude) in metro_areas:
-  if longitude <= 0:
-    print(fmt.format(name, latitude, longitude))
+    if longitude <= 0:
+        print(fmt.format(name, latitude, longitude))
 
 
 # namedtuple
@@ -91,10 +92,9 @@ for name, cc, pop, (latitude, longitude) in metro_areas:
 # 필드명이 클래스에 저장되브로 namedtuple()로 생성한 객체는 튜플과 동일한 크기의 메모리만 사용한다.
 # 속성을 객체마다 존재하는 __dict__에 저장하지 않으므로 일반적인 객체보다 메모리를 적게 사용한다.
 
-from collections import namedtuple
 
 City = namedtuple('City', 'name country population coodinates')
-tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139,691667))
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139, 691667))
 print(tokyo)
 
 print(tokyo.population)
@@ -102,3 +102,19 @@ print(tokyo.population)
 print(tokyo.coodinates)
 
 print(tokyo[1])
+
+# namedtuple은 튜플에서 상속받은 속성 외 몇 가지 속성을 더 가지고 있다.
+
+print(City._fields)
+
+LatLong = namedtuple('LatLong', 'lat long')
+
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+
+delhi = City._make(delhi_data)
+
+print(delhi._asdict())
+
+# k v 뿌리기
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
